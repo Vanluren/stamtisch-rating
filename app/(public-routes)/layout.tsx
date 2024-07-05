027f44c4ab28c4b4ba9696ac0e8d4de8819362e3
Villads Nielsen <villads.nielsen@on-running.com>
2024-07-06 01:05:58 +0200

    style(public-routes): Remove unnecessary my-32 class from main container

--- app/(public-routes)/layout.tsx
+++ app/(public-routes)/layout.tsx
@@ -4,7 +4,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="container relative h-full  my-32 flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <main className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10  lg:flex dark:border-r">
         <div className="absolute inset-0 bg-primary-foreground" />
         <section className="relative z-20 mt-auto">
