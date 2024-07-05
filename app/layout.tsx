740644183df803adc10d632cfb4b94ff89f5523c
Villads Nielsen <villads.nielsen@on-running.com>
2024-07-06 01:05:15 +0200

    feat(analytics): Add Vercel analytics integration to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = FontSans({
   subsets: ["latin"],
@@ -16,11 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <html lang="en">
-        <body className={`${inter.className} w-screen h-screen`}>
-          {children}
-        </body>
-      </html>
+      <Analytics />
+      <body className={`${inter.className} w-screen h-screen`}>{children}</body>
     </html>
   );
 }
