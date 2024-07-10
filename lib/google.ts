d019145a4f87575a13f998164ac4877d8d232fe0
Villads Nielsen <villads.nielsen@on-running.com>
2024-07-10 11:18:15 +0200

    refactor(google.ts): Refactor headers setup in googlePlacesTextSearch function

--- lib/google.ts
+++ lib/google.ts
@@ -1,19 +1,22 @@
 import { GooglePlace } from "@/types/Google";
 
-const API_KEY = process.env.GOOGLE_API_KEY;
+const API_KEY = process.env.GOOGLE_API_KEY ?? "";
 
 export async function googlePlacesTextSearch(
   textQuery: string,
 ): Promise<GooglePlace[]> {
   const url = new URL("https://places.googleapis.com/v1/places:searchText");
+  const headers = new Headers();
+  headers.set("X-Goog-Api-Key", API_KEY);
+  headers.set(
+    "X-Goog-FieldMask",
+    "places.displayName,places.formattedAddress,places.id,places.location",
+  );
+  headers.set("Content-Type", "application/json");
+
   const response = await fetch(url.toString(), {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Goog-Api-Key": API_KEY,
-      "X-Goog-FieldMask":
-        "places.displayName,places.formattedAddress,places.id,places.location",
-    },
+    headers,
     body: JSON.stringify({
       textQuery,
     }),
